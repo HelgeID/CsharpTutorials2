c84d1a2a5a771a31b44fdffa27260773df381a20
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-02 19:56:02 +0200

    Add files via upload

---
+++ list11.0.4, делегати (приклад).cs
@@ -0,0 +1,31 @@
+﻿//делегати (приклад)
+using System;
+
+namespace Delegates
+{
+    static class MyClass
+    {
+        public static void Method(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    public delegate void MyDelegate(string str);
+
+    class Program
+    {
+        static void Main()
+        {
+            //v1
+            MyDelegate myDelegate1 = new MyDelegate(MyClass.Method);
+            myDelegate1("Hello 1");//Hello 1
+
+            //v2
+            MyDelegate myDelegate2 = MyClass.Method;
+            myDelegate2("Hello 2");//Hello 2
+
+            Console.ReadKey();
+        }
+    }
+}
