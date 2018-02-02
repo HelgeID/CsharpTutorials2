ba6ad19e13e9be756779b651635c7e3c00ee21c7
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-03 01:17:34 +0200

    Add files via upload

---
+++ list12.0.2, універсальні шаблони (універсальний метод).cs
@@ -0,0 +1,16 @@
+﻿//універсальні шаблони (універсальний метод)
+    class MyClass
+    {
+        public void MyMethod<T>(T argument)
+        {
+            T variable = argument;
+            Console.WriteLine(variable);
+        }
+    }
+
+...
+MyClass ins = new MyClass();
+//спосіб 1
+ins.MyMethod<string>("Hello 1");//Hello 1
+//спосіб 2 (явно не вказуємо параметр типа)
+ins.MyMethod("Hello 2");//Hello 2
