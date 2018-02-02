7f41bde70303b778fca02f6ba7b4c2a26db0fde2
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-03 01:19:19 +0200

    Add files via upload

---
+++ list12.0.7, універсальні шаблони (частинні класи та методи).cs
@@ -0,0 +1,31 @@
+﻿//універсальні шаблони (частинні класи та методи)
+
+    //частинні класи не можуть мати out-параметри
+    public partial class MyClass<T>
+    {
+        partial void PartialMethod<T>(T a, ref T b);
+    }
+
+    public partial class MyClass<T>
+    {
+        partial void PartialMethod<T>(T a, ref T b)
+        {
+            b = default(T);
+            Console.WriteLine("{0}, {1}", a, b);
+        }
+
+        public void Proxy(T a, ref T b)
+        {
+            PartialMethod(a, ref b);
+        }
+    }
+
+...
+MyClass<int> ins = new MyClass<int>();
+int operand1 = 1, operand2 = 2;
+ins.Proxy(operand1, ref operand2);
+Console.WriteLine(operand2);
+/*
+1, 0
+0
+*/
