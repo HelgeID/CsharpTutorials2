c4a098574fc202a27f5d00d4b7711f856cade560
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-02 19:57:07 +0200

    Add files via upload

---
+++ list11.0.6, делегати, рекурсія в лямбда-операторах.cs
@@ -0,0 +1,32 @@
+﻿//делегати, рекурсія в лямбда-операторах
+using System;
+
+namespace Delegates
+{
+    delegate void MyDelegate(int index);
+
+    class Program
+    {
+        static void Main()
+        {
+            MyDelegate my = null;
+
+            my = (int i) =>
+            {
+                i--;
+                Console.WriteLine("Begin {0}", i);
+
+                if (i > 0)
+                {
+                    my(i);
+                }
+
+                Console.WriteLine("End {0}", i);
+            };
+
+            my(5);
+
+            Console.ReadKey();
+        }
+    }
+}
