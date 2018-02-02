4c0e39d21d83ae8a4925ea900ac6524ec0981a1c
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-03 01:16:18 +0200

    Add files via upload

---
+++ list12.0.0, універсальні шаблони.cs
@@ -0,0 +1,19 @@
+﻿//універсальні шаблони
+    //параметризований вказівникoм місця заповнення типом T клас MyClass
+    class MyClass<T>
+    {
+        public T myField;
+        public void MyMethod()
+        {
+            Console.WriteLine(myField.GetType());
+        }
+    }
+
+...
+MyClass<int> ins1 = new MyClass<int>();
+ins1.MyMethod();//System.Int32
+MyClass<long> ins2 = new MyClass<long>();
+ins2.MyMethod();//System.Int64
+MyClass<string> ins3 = new MyClass<string>();
+ins3.myField = "Hello, World!";
+ins3.MyMethod();//System.String
