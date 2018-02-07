151544b31a7198957a4b44f4770a2172d42160d2
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-07 16:21:02 +0200

    Add files via upload

---
+++ list16.0.3, класи-події (abstract and virtual).cs
@@ -0,0 +1,63 @@
+﻿//класи, події (abstract and virtual)
+    public delegate void EventDelegate();
+
+    interface IInterface
+    {
+        event EventDelegate MyEvent;//абстрактна подія
+    }
+
+    public class BaseClass : IInterface
+    {
+        EventDelegate myEvent = null;
+        
+        public virtual event EventDelegate MyEvent//віртуальна подія
+        {
+            add { myEvent += value; }
+            remove { myEvent -= value; }
+        }
+
+        public void InvokeEvent()
+        {
+            myEvent.Invoke();
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public override event EventDelegate MyEvent//переоприділена подія
+        {
+            add
+            {
+                base.MyEvent += value;
+                Console.WriteLine("Event base class add {0}", value.Method.Name);
+            }
+
+            remove
+            {
+                base.MyEvent -= value;
+                Console.WriteLine("Event base class remove {0}", value.Method.Name);
+            }
+        }
+    }
+
+    ...
+    //методи обробника подій
+    static private void Handler1()
+    {
+        Console.WriteLine("event 1");
+    }
+
+    static private void Handler2()
+    {
+        Console.WriteLine("event 2");
+    }
+
+
+        DerivedClass ins = new DerivedClass();
+        ins.MyEvent += new EventDelegate(Handler1);
+        ins.MyEvent += new EventDelegate(Handler2);
+        ins.InvokeEvent();//метод, що викликає подію
+
+        ins.MyEvent -= new EventDelegate(Handler2);
+        ins.InvokeEvent();//метод, що викликає подію
+    
