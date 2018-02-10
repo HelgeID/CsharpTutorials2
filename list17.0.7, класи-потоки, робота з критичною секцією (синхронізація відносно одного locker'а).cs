1bdcde3759afad17dc7f055a50e620180b521964
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-10 10:45:48 +0200

    Add files via upload

---
+++ list17.0.7, класи-потоки, робота з критичною секцією (синхронізація відносно одного locker'а).cs
@@ -0,0 +1,45 @@
+﻿//класи, потоки, робота з критичною секцією (синхронізація відносно одного locker'а)
+using System;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        //критичні секції в різних місцях, але об'єкт блокування locker - один
+        static object locker = new object();
+
+        static void WriteSecond()
+        {
+            //Stream Secondary
+            for (int counter = 0; counter < 20; counter++)
+            {
+                lock (locker)
+                {
+                    //критична секція Secondary
+                    Console.WriteLine("Stream Secondary: step {0}", counter);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart WriteSecond = new ThreadStart(Program.WriteSecond);
+            Thread thread = new Thread(Program.WriteSecond);
+            thread.Start();
+
+            //Stream Primary
+            for (int counter = 0; counter < 20; counter++)
+            {
+                lock (locker)
+                {
+                    //критична секція Primary
+                    Console.WriteLine("Stream Primary: step {0}", counter);
+                    Thread.Sleep(100);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
