25e8ad1628dd7cc55c977b22f3eb8054a4d77d38
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-10 10:43:47 +0200

    Add files via upload

---
+++ list17.0.2, класи-потоки (приклад запуску потоків).cs
@@ -0,0 +1,43 @@
+﻿//класи, потоки (приклад запуску потоків)
+using System;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void WriteSecond()
+        {
+            Thread thread = Thread.CurrentThread;//повертає посилання на екземпляр поточного потоку
+            thread.Name = "Secondary";//присвоюємо імя потоку
+            //виводимо інформацію про поточний поток
+            Console.WriteLine("ID {0}: {1}", thread.Name, thread.GetHashCode());
+
+            //робота поточного потоку
+            for (int counter = 0; counter < 10; counter++) {
+                Console.WriteLine(new string(' ', 10) + thread.Name + " " + counter);
+                Thread.Sleep(1000);//призупиняємо виконання поточного потоку
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread primaryThread = Thread.CurrentThread;//получаємо посилання на екземпляр поточного потоку
+            primaryThread.Name = "Primary";//присвоюємо імя потоку
+            //виводимо інформацію про поточний поток
+            Console.WriteLine("ID {0}: {1}", primaryThread.Name, primaryThread.GetHashCode());
+
+            //робота вторинного потока
+            Thread secondaryThread = new Thread(WriteSecond);
+            secondaryThread.Start();
+
+            //робота первинного потока
+            for (int counter = 0; counter < 10; counter++) {
+                Console.WriteLine(primaryThread.Name + " " + counter);
+                Thread.Sleep(1000);//призупиняємо виконання поточного потоку
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
