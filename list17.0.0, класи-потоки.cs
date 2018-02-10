754d9b44b7228b96f1e146ba9152de09bcd1e021
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-10 10:42:57 +0200

    Add files via upload

---
+++ list17.0.0, класи-потоки.cs
@@ -0,0 +1,31 @@
+﻿//класи, потоки
+using System;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        //вторинний поток
+        static void WriteSecond()
+        {
+            while (true) {
+                Console.WriteLine(new string(' ', 10) + "Secondary");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart writeSecond = new ThreadStart(WriteSecond);//writeSecond - делегат, для того, щоб метод WriteSecond виконувався паралельно
+            Thread thread = new Thread(writeSecond);//створюємо обєктне-орієнтоване представлення окремого потока
+            thread.Start();
+
+            //первинний поток
+            while (true) {
+                Console.WriteLine("Primary");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
