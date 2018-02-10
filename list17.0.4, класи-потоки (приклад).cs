d2734fcf72d0e794d796a78ccd9560f997bcc23d
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-10 10:44:33 +0200

    Add files via upload

---
+++ list17.0.4, класи-потоки (приклад).cs
@@ -0,0 +1,26 @@
+﻿//класи, потоки (приклад)
+using System;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ThreadStart
+            int counter = 0;
+            Thread thread = new Thread(delegate () { Console.WriteLine("1. counter = {0}", ++counter); });
+            thread.Start();
+
+            Thread.Sleep(100);
+            Console.WriteLine("2. counter = {0}", counter);
+
+            //ParameterizedThreadStart
+            thread = new Thread((object argument) => { Console.WriteLine("3. counter = {0}", (int)argument); });
+            thread.Start(counter);
+
+            Console.ReadKey();
+        }
+    }
+}
