612ef175f55be083c0ff776214766c868be8198e
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-10 10:44:13 +0200

    Add files via upload

---
+++ list17.0.3, класи-потоки (передача параметрів, групи параметрів).cs
@@ -0,0 +1,45 @@
+﻿//класи, потоки (передача параметрів, групи параметрів)
+using System;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    class Box
+    {
+        public string el1 = "Hello!";
+        public int el2 = 100;
+    }
+
+    class Program
+    {
+        static void WriteBox(object argument)
+        {
+            Box box = (Box)argument;
+            Console.WriteLine(box.el1);
+            Console.WriteLine(box.el2);
+        }
+
+        static void WriteSecond(object argument)
+        {
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine(argument);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ParameterizedThreadStart writeBox = new ParameterizedThreadStart(WriteBox);
+            Thread threadBox = new Thread(writeBox);
+            threadBox.Start(new Box());//передача Box, групи параметрів
+
+            Thread.Sleep(1000);//робимо затримку 1с
+
+            ParameterizedThreadStart writeSecond = new ParameterizedThreadStart(WriteSecond);
+            Thread thread = new Thread(writeSecond);
+            thread.Start("Hi!");//передаа параметра "Hi!"
+
+            Console.ReadKey();
+        }
+    }
+}
