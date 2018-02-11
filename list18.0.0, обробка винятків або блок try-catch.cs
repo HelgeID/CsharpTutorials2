0fc57c5ea2568fb913ed01b0e4a18c583b873370
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-11 18:30:57 +0200

    Add files via upload

---
+++ list18.0.0, обробка винятків або блок try-catch.cs
@@ -0,0 +1,28 @@
+﻿//обробка винятків або блок try-catch
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int n = 2;
+
+            try
+            {
+                a = a / (2 - n);
+                Console.WriteLine("a = {0}", a);
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.GetType());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
