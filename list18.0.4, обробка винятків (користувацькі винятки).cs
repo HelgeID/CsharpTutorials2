aff4dc7a05875cc122f95b2e00c1225b43d3f33f
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-11 18:34:23 +0200

    Add files via upload

---
+++ list18.0.4, обробка винятків (користувацькі винятки).cs
@@ -0,0 +1,32 @@
+﻿//обробка винятків (користувацькі винятки)
+using System;
+
+namespace ConsoleApplication
+{
+    //для створення користувацьких винятків, треба наслідувати від System.Exception
+    class UserException : Exception
+    {
+        public void Method()
+        {
+            Console.WriteLine("my exception");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new UserException();
+            }
+
+            catch (UserException e)
+            {
+                e.Method();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
