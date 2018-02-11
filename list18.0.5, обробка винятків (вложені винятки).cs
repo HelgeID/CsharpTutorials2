e3bed3d7b4bf9f15ae8a2b0b3dd94b7601803616
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-11 18:35:18 +0200

    Add files via upload

---
+++ list18.0.5, обробка винятків (вложені винятки).cs
@@ -0,0 +1,43 @@
+﻿//обробка винятків (вложені винятки)
+using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    class UserException : Exception
+    {
+        public void Method()
+        {
+            Console.WriteLine("my exception");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new UserException();
+            }
+
+            catch (UserException userException)
+            {
+                userException.Method();
+
+                try
+                {
+                    FileStream fs = File.Open(@"C:\file.log", FileMode.Open);
+                }
+
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    Console.WriteLine(exception.GetType());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
