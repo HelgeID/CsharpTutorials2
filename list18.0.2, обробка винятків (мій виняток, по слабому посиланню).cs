a0e65fcaa433300f28c9e92dd49fdbdc1a3ce882
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-11 18:33:24 +0200

    Add files via upload

---
+++ list18.0.2, обробка винятків (мій виняток, по слабому посиланню).cs
@@ -0,0 +1,23 @@
+﻿//обробка винятків (мій виняток, по слабому посиланню)
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception("my exception");
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);//my exception
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
