6c10df499a7ff5f18e81cb35d328a1f39896fa6b
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-12 09:42:55 +0200

    Add files via upload

---
+++ list18.0.6, обробка винятків (finally).cs
@@ -0,0 +1,37 @@
+﻿//обробка винятків (finally)
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int n = 2;
+
+            try
+            {
+                a = a / (2 - n);
+                Console.WriteLine("a = {0}", a);
+            }
+            
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.GetType());
+            }
+
+            //блок finally - виконується завжди
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
