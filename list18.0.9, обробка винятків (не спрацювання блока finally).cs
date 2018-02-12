a4375769a7eab636752b2c195b67d57560fd2ae4
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-12 09:44:11 +0200

    Add files via upload

---
+++ list18.0.9, обробка винятків (не спрацювання блока finally).cs
@@ -0,0 +1,67 @@
+﻿//обробка винятків (не спрацювання блока finally)
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        public static void Method()
+        {
+            //блок finally - не спрацює, System.StackOverflowException
+            //int[] arr = new int[10];
+            //Console.WriteLine(arr);
+            //Method();
+
+            //блок finally - спрацює, System.OutOfMemoryException
+            //int[] arr = new int[100000000];
+            //Console.WriteLine(arr);
+            //Method();
+        }
+
+        static void Main()
+        {
+            try
+            {
+                Method();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+//блок finally не спрацює
+
+try
+{
+    throw new Exception();
+}
+
+catch (Exception)
+{
+    //v1
+    throw new Exception();
+
+    //v2
+    while (true)
+        Console.WriteLine("catch");
+}
+
+finally
+{
+    while (true)
+        Console.WriteLine("finally");
+}
+
+*/
