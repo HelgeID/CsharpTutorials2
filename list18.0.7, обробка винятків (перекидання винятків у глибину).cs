915b00b2fb3bc59c4e8200f45af699f0b2efc5b1
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-12 09:43:26 +0200

    Add files via upload

---
+++ list18.0.7, обробка винятків (перекидання винятків у глибину).cs
@@ -0,0 +1,52 @@
+﻿//обробка винятків (перекидання винятків у глибину)
+using System;
+
+namespace ConsoleApplication
+{
+    class UserException : Exception
+    {
+        public void Method()
+        {
+            Console.WriteLine("my exception");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new UserException();
+            }
+
+            catch (UserException userException)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("обробка винятка 1:");
+                userException.Method();
+
+                try
+                {
+                    //перекидаємо ще раз виняток
+                    throw userException;
+                }
+
+                catch (UserException exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("обробка винятка 2:");
+                    exception.Method();
+                }
+            }
+
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
