a46fad481f51fbef07396f15ab1927338406f705
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-12 09:44:30 +0200

    Add files via upload

---
+++ list18.1.0, обробка винятків (система вкладених винятків).cs
@@ -0,0 +1,51 @@
+﻿//обробка винятків (система вкладених винятків)
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                try
+                {
+                    try
+                    {
+                        throw new Exception("Exception");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Catch 1: " + e.Message);//Exception
+                        throw;
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Finally 1");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Catch 2: " + e.Message);//Exception
+                    throw;
+                }
+                finally
+                {
+                    Console.WriteLine("Finally 2");
+                    throw new Exception("New Exception");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Catch 3: " + e.Message);//New Exception
+            }
+            finally
+            {
+                Console.WriteLine("Finally 3");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
