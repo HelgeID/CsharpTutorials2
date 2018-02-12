0d5074c2d089eb7868e7cfa5d1c0c6ce167b6766
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-12 09:45:18 +0200

    Add files via upload

---
+++ list18.1.2, обробка винятків (порядок обробки).cs
@@ -0,0 +1,51 @@
+﻿//обробка винятків (порядок обробки)
+using System;
+
+namespace ConsoleApplication
+{
+    class MyExceptionA : Exception
+    {
+        public MyExceptionA(string message)
+            :base(message)
+        {
+        }
+    }
+
+    class MyExceptionB : MyExceptionA
+    {
+        public MyExceptionB(string message)
+            : base(message)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                //throw new Exception("Exception");
+                throw new MyExceptionA("MyExceptionA");
+                //throw new MyExceptionB("MyExceptionB");
+            }
+
+            //catch'і є у зворотньому порядку від наслідування:
+            //MyExceptionB->MyExceptionA->Exception
+            catch (MyExceptionB e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (MyExceptionA e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
