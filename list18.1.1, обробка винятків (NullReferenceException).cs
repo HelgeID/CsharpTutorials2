05f56f938ac3501a114d0d4e5a37ba58cba22c07
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-02-12 09:45:03 +0200

    Add files via upload

---
+++ list18.1.1, обробка винятків (NullReferenceException).cs
@@ -0,0 +1,39 @@
+﻿//обробка винятків (NullReferenceException)
+using System;
+
+namespace ConsoleApplication
+{
+    //class MyClass
+    //{
+    //    public void Method()
+    //    {
+    //        Console.WriteLine("My Method");
+    //    }
+    //}
+
+    class Program
+    {
+        static void Main()
+        {
+            //v1
+            //MyClass my = null;
+            //my.Method();//System.NullReferenceException
+
+            //v2
+            try
+            {
+                throw null;
+                //или
+                //Exception my = null;
+                //throw my;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.GetType());//System.NullReferenceException
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
