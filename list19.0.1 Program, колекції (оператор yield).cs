4712ae3045da87ce77377f654d8b407d279a78fa
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-03-20 22:13:39 +0200

    Add files via upload

---
+++ list19.0.1 Program, колекції (оператор yield).cs
@@ -0,0 +1,32 @@
+﻿//колекції (оператор yield)
+
+//Program.cs
+using System;
+using System.Collections;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            //v1 
+            foreach (string element in UserCollection.Power())
+                Console.WriteLine(element);
+            Console.WriteLine(new string('-', 10));
+
+
+
+            //v2 (так працює foreach)
+            IEnumerable enumerable = UserCollection.Power();
+            IEnumerator enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                String element = enumerator.Current as String;
+                Console.WriteLine(element);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ list19.0.1 UserCollection, колекції (оператор yield).cs
@@ -0,0 +1,26 @@
+﻿//UserCollection.cs
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    class UserCollection
+    {
+        public static IEnumerable Power()
+        {
+            //yield - генератор програмних кодів колекцій (озн. повертати, видавати)
+            yield return "Hello world!";
+
+            //yield return "1";
+            //yield return "2";
+            //yield return "3";
+
+            //yield break;//нічого не повертає
+
+            //while (true)
+            //    yield return "Hello world!";
+        }
+    }
+}
