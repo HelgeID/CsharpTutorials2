5effd3ad3b8fc0e50936a8db48dc9ce84f0bb22c
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-03-21 11:54:47 +0200

    Add files via upload

---
+++ list20.0.4, базовий клас Object (метод GetType()).cs
@@ -0,0 +1,16 @@
+﻿//базовий клас Object (метод GetType())
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Object obj = new Object();
+            Type type = obj.GetType();
+            Console.WriteLine(type.ToString());//System.Object
+            Console.ReadKey();
+        }
+    }
+}
