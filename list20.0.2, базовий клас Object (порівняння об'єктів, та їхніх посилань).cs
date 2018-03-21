bc5f127f62b5912cc0dfb93edb9360c3cc0990b4
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-03-21 11:53:57 +0200

    Add files via upload

---
+++ list20.0.2, базовий клас Object (порівняння об'єктів, та їхніх посилань).cs
@@ -0,0 +1,26 @@
+﻿//базовий клас Object (порівняння об'єктів, та їхніх посилань)
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Object obj1 = new Object();
+            Object obj2 = new Object();
+
+            //Різні хеш-коди
+            Console.WriteLine(obj1.Equals(obj2));//False
+            Console.WriteLine(ReferenceEquals(obj1, obj2));//False
+            Console.WriteLine(Equals(obj1, obj2));//False
+
+            obj1 = obj2;
+            Console.WriteLine(obj1.Equals(obj2));//True
+            Console.WriteLine(ReferenceEquals(obj1, obj2));//True
+            Console.WriteLine(Equals(obj1, obj2));//True
+
+            Console.ReadKey();
+        }
+    }
+}
