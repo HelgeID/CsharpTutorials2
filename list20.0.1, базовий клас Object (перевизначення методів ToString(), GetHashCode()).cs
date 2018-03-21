93af9aba132c6a3aec323e2c23e553a7e665699f
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-03-21 11:52:53 +0200

    Add files via upload

---
+++ list20.0.1, базовий клас Object (перевизначення методів ToString(), GetHashCode()).cs
@@ -0,0 +1,34 @@
+﻿//базовий клас Object (перевизначення методів ToString(), GetHashCode())
+using System;
+
+namespace ConsoleApplication
+{
+    class MyClass : Object
+    {
+        //перевизначення методів, бо методи ToString(), GetHashCode() - віртуальні
+
+        public override string ToString()
+        {
+            return "Hello, World!";
+        }
+
+        //!ps: бажано виконувати перевизначення разом в парі з Equals()
+        public override int GetHashCode()
+        {
+            return 1234567890;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass ins = new MyClass();
+            Console.WriteLine(ins.ToString());//Hello, World!
+            Console.WriteLine(ins);//Hello, World!
+            Console.WriteLine(ins.GetHashCode());//1234567890
+
+            Console.ReadKey();
+        }
+    }
+}
