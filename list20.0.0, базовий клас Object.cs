23c0ad16dac932afaf3a48d3495f8e1af23ac12c
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-03-21 11:52:00 +0200

    Add files via upload

---
+++ list20.0.0, базовий клас Object.cs
@@ -0,0 +1,27 @@
+﻿//базовий клас Object
+using System;
+
+namespace ConsoleApplication
+{
+    //повне ім'я
+    class MyClassA: Object
+    {
+    }
+
+    //аліас (псевдонім)
+    class MyClassB: object
+    {
+    }
+
+    //!!! ps: наслідування тут відбувається неявно, тому
+    //": Object" і ": object" можна закоментувати!!!
+    class Program
+    {
+        static void Main()
+        {
+            Object insA = new MyClassA();
+            object insB = new MyClassB();
+            Console.ReadKey();
+        }
+    }
+}
