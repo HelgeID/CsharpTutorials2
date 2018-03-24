306c1b8348f68062c997c019cb2b5e0bcea0c770
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-03-24 11:48:35 +0200

    Add files via upload

---
+++ list20.0.5, базовий клас Object (клонування).cs
@@ -0,0 +1,34 @@
+﻿//базовий клас Object (клонування)
+using System;
+
+namespace ConsoleApplication
+{
+    class MyClass
+    {
+        public static string str = "Hello, World!";
+        public int number;
+    }
+
+    class Program : MyClass
+    {
+        static void Main()
+        {
+            Program original = new Program();
+            original.number = 100;
+            Console.WriteLine(Program.str + " " + original.number);//Hello, World! 100
+
+            //клонування
+            Program clone = original.MemberwiseClone() as Program;//створюємо в памяті копію оригіналу(клон)
+            Console.WriteLine(Program.str + " " + clone.number);//Hello, World! 100
+
+            //перевірка
+            clone.number = 200;
+            Program.str = "HI!";
+
+            Console.WriteLine(Program.str + " " + original.number);//HI! 100
+            Console.WriteLine(Program.str + " " + clone.number);//HI! 200
+
+            Console.ReadKey();
+        }
+    }
+}
