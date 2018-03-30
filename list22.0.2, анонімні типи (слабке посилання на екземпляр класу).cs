d64dd88812d63d975189d63e4e9dc6ebc774d162
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-03-30 11:34:17 +0300

    Add files via upload

---
+++ list22.0.2, анонімні типи (слабке посилання на екземпляр класу).cs
@@ -0,0 +1,28 @@
+﻿//анонімні типи (слабке посилання на екземпляр класу)
+using System;
+
+namespace ConsoleApplication
+{
+    class MyClass
+    {
+        public int field;
+
+        public void Method()
+        {
+            Console.WriteLine(field);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            new
+            {
+                My = new MyClass { field = 100 }
+            }.My.Method();//100
+
+            Console.ReadKey();
+        }
+    }
+}
