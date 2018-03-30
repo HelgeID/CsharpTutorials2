fa829ae22b55639790ba38ed83143f2680e8ff33
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-03-30 11:35:12 +0300

    Add files via upload

---
+++ list22.0.3, анонімні типи (використання делегатів).cs
@@ -0,0 +1,20 @@
+﻿//анонімні типи (використання делегатів)
+using System;
+
+namespace ConsoleApplication
+{
+    delegate void MyDelegate(string @string);
+    class Program
+    {
+        static void Main()
+        {
+            var ins = new
+            {
+                MyDel = new MyDelegate((string @string) => Console.WriteLine(@string))
+            };
+
+            ins.MyDel("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
