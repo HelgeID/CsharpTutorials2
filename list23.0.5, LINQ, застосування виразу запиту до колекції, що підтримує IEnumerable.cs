3aefb1d03df79ffeaa099736822d4bdb8212f300
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-06 11:44:45 +0300

    Add files via upload

---
+++ list23.0.5, LINQ, застосування виразу запиту до колекції, що підтримує IEnumerable.cs
@@ -0,0 +1,34 @@
+﻿//LINQ, застосування виразу запиту до колекції, що підтримує IEnumerable
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            ArrayList numbers = new ArrayList();
+            numbers.Add(1);
+            numbers.Add(2);
+            //явне вказання типа Int32 змінній діапазону n
+            //var - неможливо так, як IEnumerable - не параметризований
+            var query = from int n in numbers
+                        select n * 2;//select - операція проекції
+            foreach (var item in query)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+    використання анонімних типів
+    ...
+    select new { Digit = n * 2 };
+
+    ...
+    item.Digit
+ */
