9a9f3aac07f190629bc20e789d40264dedacab19
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-14 07:57:07 +0300

    Add files via upload

---
+++ list23.0.9, LINQ, select - операція проекції.cs
@@ -0,0 +1,24 @@
+﻿//LINQ, select - операція проекції
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+
+            //будуємо запит (заміна класа анонімний типом)
+            var query = from x in numbers
+                        select new { Input = x, Output = x * 2 };
+
+            foreach (var item in query)
+                Console.WriteLine("Input = {0}, \tOutput = {1}", item.Input, item.Output);
+
+            Console.ReadKey();
+        }
+    }
+}
