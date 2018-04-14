e1d54525e59423cf1e780e251e108a236aeecc1a
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-14 07:56:45 +0300

    Add files via upload

---
+++ list23.0.8, LINQ, select - операція проекції.cs
@@ -0,0 +1,38 @@
+﻿//LINQ, select - операція проекції
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Result
+    {
+        public Result(int input, int output)
+        {
+            Input = input;
+            Output = output;
+        }
+
+        public int Input { get; set; }
+        public int Output { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+            var query = from x in numbers
+                        select new Result(x, x * 2);
+
+            numbers[0] = 100;
+
+            //будуємо запит 
+            //select new Result(x, x * 2); - починає працювати, коли ми звертаємось до query
+            foreach (var item in query)
+                Console.WriteLine("Input = {0}, \tOutput = {1}", item.Input, item.Output);
+
+            Console.ReadKey();
+        }
+    }
+}
