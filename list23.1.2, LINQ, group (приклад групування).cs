d261a84113d9425a171c2d8e3ffc965007f2a322
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-14 07:58:20 +0300

    Add files via upload

---
+++ list23.1.2, LINQ, group (приклад групування).cs
@@ -0,0 +1,44 @@
+﻿//LINQ, group(приклад групування)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Employee
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Nationality { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee { LastName = "Andreev", FirstName = "Andrew", Nationality = "uk" },
+                new Employee { LastName = "Ivanov", FirstName = "Ivan", Nationality = "ru" },
+                new Employee { LastName = "Andreev", FirstName = "Ivan", Nationality = "uk" },
+                new Employee { LastName = "Petrov", FirstName = "Petr", Nationality = "en" },
+                new Employee { LastName = "Andreev", FirstName = "Sergey", Nationality = "uk" },
+                new Employee { LastName = "Petrov", FirstName = "Slava", Nationality = "en" }
+            };
+
+            //будуємо запит
+            var query = from emp in employees
+                        group emp by new { Nationality = emp.Nationality, LastName = emp.LastName };
+
+            foreach (var group in query)
+            {
+                Console.WriteLine(group.Key);
+
+                foreach (var employee in group)
+                    Console.WriteLine(employee.FirstName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
