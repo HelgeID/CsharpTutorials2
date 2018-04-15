7db06b72d4ef83e51462e132647faa7ba1dda056
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-15 08:37:29 +0300

    Add files via upload

---
+++ list24.0.0, класи - глобальний простір імен, директива using.cs
@@ -0,0 +1,12 @@
+﻿//класи - глобальний простір імен, директива using
+
+//using System;
+class Program
+{
+    static void Main()
+    {
+        //Console.WriteLine(100);//error - без директиви using не працює
+        System.Console.WriteLine("Hello, World!");
+        System.Console.ReadKey();
+    }
+}
