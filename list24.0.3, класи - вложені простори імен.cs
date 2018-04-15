b3033d70c451b9b6024577fce4357af13a07d822
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-15 08:38:45 +0300

    Add files via upload

---
+++ list24.0.3, класи - вложені простори імен.cs
@@ -0,0 +1,34 @@
+﻿//класи - вложені простори імен
+
+namespace namesA
+{
+    namespace namesB
+    {
+        namespace namesC
+        {
+            //MyClass
+            class MyClass
+            {
+                public MyClass()
+                {
+                    System.Console.WriteLine(this.GetType().Name);
+                }
+            }
+        }
+    }
+}
+
+namespace MyNamespace
+{
+    using namesA.namesB.namesC;
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass my = new MyClass();
+
+            System.Console.ReadKey();
+        }
+    }
+}
