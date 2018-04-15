8f8ff6cc2148c1f93234b44b30a91f30e586ab1c
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-15 08:38:20 +0300

    Add files via upload

---
+++ list24.0.2 Program, класи - власний простір імен (повне кваліфіковане ім'я).cs
@@ -0,0 +1,13 @@
+﻿//класи - власний простір імен, повне кваліфіковане ім'я
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyNamespace.MyClass my = new MyNamespace.MyClass();
+            System.Console.ReadKey();
+        }
+    }
+}
