a46d2a27f169a7bd23fdb634638aa2354aa13682
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-15 08:39:09 +0300

    Add files via upload

---
+++ list24.0.4, класи - простор імен, псевдоніми типів.cs
@@ -0,0 +1,38 @@
+﻿//класи - простор імен, псевдоніми типів
+
+namespace namesA
+{
+    namespace namesB
+    {
+        namespace namesC
+        {
+            class MyClassC
+            {
+                public void Method()
+                {
+                    System.Console.WriteLine("Hello, World!");
+                }
+            }
+        }
+        class MyClassB { }
+    }
+    class MyClassA { }
+}
+
+namespace MyNamespace
+{
+    //MyClass - це псевдонім для MyClassC
+    using MyClass = namesA.namesB.namesC.MyClassC;
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass my = new MyClass();
+            my.Method();
+            System.Console.WriteLine(my.GetType());//повертає імя MyClassC
+
+            System.Console.ReadKey();
+        }
+    }
+}
