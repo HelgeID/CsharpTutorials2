fc05f1556f26da1cb07c61c3710485e1b4206a23
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-15 08:37:53 +0300

    Add files via upload

---
+++ list24.0.1 MyClass, класи - власний простір імен.cs
@@ -0,0 +1,13 @@
+﻿//MyClass.cs
+using System;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public MyClass()
+        {
+            Console.WriteLine("Constructor - MyClass");
+        }
+    }
+}
---
+++ list24.0.1 Program, класи - власний простір імен.cs
@@ -0,0 +1,12 @@
+﻿//класи - власний простір імен
+
+using MyNamespace;
+
+class Program
+{
+    static void Main()
+    {
+        MyClass my = new MyClass();
+        System.Console.ReadKey();
+    }
+}
