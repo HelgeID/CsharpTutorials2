36b349b201b3afd13908d008c0efcf39d5cc91b3
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-16 18:54:01 +0300

    Add files via upload

---
+++ list24.0.7, класи - простори імен (error StackOverflowException пр-1).cs
@@ -0,0 +1,31 @@
+﻿//класи - простори імен (error StackOverflowException)
+
+namespace namesA
+{
+    class MyClassA
+    {
+        MyClassA my;
+
+        public MyClassA()
+        {
+            System.Console.WriteLine("Constructor MyClassA");
+
+            //рефлексивна самоасоціація
+            my = new MyClassA();
+        }
+    }
+}
+
+namespace ConsoleApplication
+{
+    using namesA;
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClassA myA = new MyClassA();
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ list24.0.8, класи - простори імен (error StackOverflowException пр-2).cs
@@ -0,0 +1,50 @@
+﻿//класи - простори імен (error StackOverflowException)
+using System;
+
+using namesA;
+using namesA.namesB;
+using namesA.namesB.namesC;
+
+namespace namesA
+{
+    namespace namesB
+    {
+        namespace namesC
+        {
+            class MyClassC
+            {
+                MyClassA myA = new MyClassA(); //StackOverflowException 3
+                MyClassB myB = new MyClassB(); //StackOverflowException 6
+                MyClassC myC = new MyClassC(); //StackOverflowException 7
+            }
+        }
+        class MyClassB
+        {
+            MyClassA myA = new MyClassA(); //StackOverflowException 4
+            MyClassB myB = new MyClassB(); //StackOverflowException 5
+            MyClassC myC = new MyClassC();
+        }
+    }
+    class MyClassA
+    {
+        MyClassA myA = new MyClassA(); //StackOverflowException 1
+        MyClassB myB = new MyClassB(); //StackOverflowException 2
+        MyClassC myC = new MyClassC();
+    }
+}
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            //видимі тому, що add (using namesA; using namesA.namesB; using namesA.namesB.namesC;)
+            MyClassA myA = new MyClassA();
+            MyClassB myB = new MyClassB();
+            MyClassC myC = new MyClassC();
+
+            Console.ReadKey();
+        }
+    }
+}
