bd09f3ec1df0d3e8f722e012ffa36329383fbdcb
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-16 18:54:44 +0300

    Add files via upload

---
+++ list24.0.9, класи - простори імен (partition).cs
@@ -0,0 +1,24 @@
+﻿//класи - простори імен (partition)
+
+namespace MyNames
+{
+    class MyClass { }
+}
+
+namespace MyNames
+{
+    //class MyClass { } //error
+}
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyNames.MyClass my = new MyNames.MyClass();
+
+            System.Console.ReadKey();
+        }
+    }
+}
