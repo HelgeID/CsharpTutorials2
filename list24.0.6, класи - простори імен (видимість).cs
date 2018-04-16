22774d5f09bb85e25bd0c24be78078a20a6ace1a
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-16 18:53:11 +0300

    Add files via upload

---
+++ list24.0.6, класи - простори імен (видимість).cs
@@ -0,0 +1,41 @@
+﻿//класи - простори імен (видимість)
+using System;
+
+namespace namesA
+{
+    namespace namesB
+    {
+        namespace namesC
+        {
+            class MyClassC
+            {
+                MyClassA myA = new MyClassA(); //ok
+                MyClassB myB = new MyClassB(); //ok
+                MyClassC myC = new MyClassC(); //ok
+            }
+        }
+        class MyClassB
+        {
+            MyClassA myA = new MyClassA(); //ok
+            MyClassB myB = new MyClassB(); //ok
+            //MyClassC myC = new MyClassC(); //недоступний
+        }
+    }
+    class MyClassA
+    {
+        MyClassA myA = new MyClassA(); //ok
+        //MyClassB myB = new MyClassB(); //недоступний
+        //MyClassC myC = new MyClassC(); //недоступний
+    }
+}
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadKey();
+        }
+    }
+}
