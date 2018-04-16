351f31142c85c1d0f21edc474ac3d1470e716f9b
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-16 18:55:10 +0300

    Add files via upload

---
+++ list24.1.0, класи - однойменні простори імен.cs
@@ -0,0 +1,25 @@
+﻿//класи - однойменні простори імен
+
+namespace MyNames
+{
+    class MyClass { }
+
+    namespace MyNames
+    {
+        class MyClass { }
+    }
+}
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyNames.MyClass my1 = new MyNames.MyClass();
+            MyNames.MyNames.MyClass my2 = new MyNames.MyNames.MyClass();
+
+            System.Console.ReadKey();
+        }
+    }
+}
