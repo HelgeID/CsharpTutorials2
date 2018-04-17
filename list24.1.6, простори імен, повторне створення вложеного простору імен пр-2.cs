f31abf8ec866c3879a5784f7feb2dc93c22b0e02
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-17 13:06:41 +0300

    Add files via upload

---
+++ list24.1.6, простори імен, повторне створення вложеного простору імен пр-2.cs
@@ -0,0 +1,39 @@
+﻿//простори імен, повторне створення вложеного простору імен
+
+namespace namesA
+{
+    namespace namesB
+    {
+        namespace namesC
+        {
+            class MyClassC
+            {
+                MyClassA myA = new MyClassA();
+                MyClassB myB = new MyClassB();
+                MyClassB2 myB2 = new MyClassB2();
+            }
+        }
+        class MyClassB { }
+    }
+    class MyClassA { }
+}
+
+//розширення namespace namesB
+namespace namesA.namesB
+{
+    class MyClassB2 { }
+}
+
+namespace ConsoleApplication
+{
+    using namesA.namesB.namesC;
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClassC myC = new MyClassC();
+            System.Console.ReadKey();
+        }
+    }
+}
