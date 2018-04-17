339bb172c7564cad26afa541a3d5f452aa54db63
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-17 10:43:18 +0300

    Add files via upload

---
+++ list24.1.2, класи - доступ між просторами імен.cs
@@ -0,0 +1,39 @@
+﻿//класи - доступ між просторами імен
+
+namespace MyNames
+{
+    namespace NamesA
+    {
+        class MyClassA
+        {
+            //без інпорта простора імен неможливо реалізувати
+            //MyClassB my = new MyClassB(); //error
+        }
+    }
+
+    namespace NamesB
+    {
+        class MyClassB
+        {
+            //без інпорта простора імен неможливо реалізувати
+            //MyClassA my = new MyClassA(); //error
+        }
+    }
+}
+
+namespace ConsoleApplication
+{
+    using MyNames.NamesA;
+    using MyNames.NamesB;
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClassA myA = new MyClassA();
+            MyClassB myB = new MyClassB();
+
+            System.Console.ReadKey();
+        }
+    }
+}
