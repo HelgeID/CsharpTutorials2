dc32d1723fadcc744b69c88ff7b3ac801070e2db
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-17 10:43:49 +0300

    Add files via upload

---
+++ list24.1.3, класи - доступ між просторами імен, при допомозі наслідування.cs
@@ -0,0 +1,35 @@
+﻿//класи - доступ між просторами імен, при допомозі наслідування
+
+namespace MyNames
+{
+    namespace NamesA
+    {
+        class Base
+        {
+        }
+    }
+
+    namespace NamesB
+    {
+        class Derived: NamesA.Base
+        {
+        }
+    }
+}
+
+namespace ConsoleApplication
+{
+    using MyNames.NamesA;
+    using MyNames.NamesB;
+
+    class Program
+    {
+        static void Main()
+        {
+            Base ins1 = new Base();
+            Derived ins2 = new Derived();
+
+            System.Console.ReadKey();
+        }
+    }
+}
