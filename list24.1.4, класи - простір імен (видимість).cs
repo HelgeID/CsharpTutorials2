7ce0465ab25afcc47e73017efbd3b10f6c67a9a0
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-17 10:44:45 +0300

    Add files via upload

---
+++ list24.1.4, класи - простір імен (видимість).cs
@@ -0,0 +1,33 @@
+﻿//класи - простір імен (видимість)
+
+namespace NamesA
+{
+    namespace NamesB
+    {
+        class MyClass { } //однойменний стереотип
+
+        class MyClassB
+        {
+            MyClass my1;
+            NamesA.MyClass my2; //пряма видимість відсутня
+            MyClassA my3; //пряма видимість є
+        }
+    }
+
+    class MyClass { } //однойменний стереотип
+    class MyClassA { }
+}
+
+namespace ConsoleApplication
+{
+    using NamesA;
+    using NamesA.NamesB;
+
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.ReadKey();
+        }
+    }
+}
