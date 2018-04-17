792ee026558d42934b10a67209558476ba7ac685
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-04-17 10:42:24 +0300

    Add files via upload

---
+++ list24.1.1, класи - однойменні простори імен (колізія між класами).cs
@@ -0,0 +1,32 @@
+﻿//класи - однойменні простори імен (колізія між класами)
+
+namespace MyNames
+{
+    class MyClass { }
+
+    namespace MyNames
+    {
+        class MyClass { }
+    }
+}
+
+namespace ConsoleApplication
+{
+    using MyNames;
+    using MyNames.MyNames;
+
+    class Program
+    {
+        static void Main()
+        {
+            //екземпляр класа без повної кваліфікації імені
+            //MyClass my = new MyClass();//error
+
+            //повна кваліфікація імені
+            MyNames.MyClass my1 = new MyNames.MyClass();
+            MyNames.MyNames.MyClass my2 = new MyNames.MyNames.MyClass();
+
+            System.Console.ReadKey();
+        }
+    }
+}
